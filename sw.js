053cbbfc1d88f35a6c29316c362296583f9dda1b
kokeshicamera-debug <kokeshicamera@gmail.com>
2026-08-10 07:43:46 +0900

    住所表示と老眼モードの操作性を改善

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * 一度オンラインで開いたアプリ本体を端末に保管し、圏外でも起動できるようにする。
  * POTA/SOTAの判定データは、次の段階で地域別にここへ追加する。
  */
-const CACHE_NAME = "cho-keiryo-log-v1-10-shell-7";
+const CACHE_NAME = "cho-keiryo-log-v1-10-shell-8";
 const APP_SHELL = [
   "./",
   "./index.html",
